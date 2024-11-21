f032a1211a3fb43dedaf63c85ba4a7a74cb677fe
Glebagee <glebsevostanov37@gmail.com>
2024-11-21 17:27:14 +0900

    Try...catch...finally
    Create a TCF block

---
+++ Try catch finally.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Введите число для деления 100:");
+
+        try
+        {
+            string userInput = Console.ReadLine();
+            int number = int.Parse(userInput); // Попытка преобразовать ввод в целое число
+            int result = 100 / number; 
+            Console.WriteLine($"Результат деления 100 на {number} равен {result}.");
+        }
+        catch (DivideByZeroException ex)
+        {
+            Console.WriteLine("Ошибка: Деление на ноль недопустимо.");
+        }
+        catch (FormatException ex)
+        {
+            Console.WriteLine("Ошибка: Введено некорректное значение. Пожалуйста, введите целое число.");
+        }
+        catch (Exception ex) // Обработка всех остальных исключений
+        {
+            Console.WriteLine($"Произошла ошибка: {ex.Message}");
+        }
+        finally
+        {
+            Console.WriteLine("Завершение программы. Спасибо за использование!");
+        }
+    }
+}
